a293d14a61f96bcca949052db8357c17f94b11d2
Tan-ming-kai <mingkai.tan@my.jcu.edu.au>
2025-08-06 10:31:46 +0800

    full code

--- Assignment/app/src/main/java/com/example/assignment1/model/Goal.kt
+++
@@ -1,7 +0,0 @@
-package com.example.assignment1.model
-
-data class Goal(
-    val id: Int = 0,
-    val description: String,
-    val amount: Double
-)
--- Assignment/app/src/main/java/com/example/assignment1/model/Transaction.kt
+++
@@ -1,9 +0,0 @@
-package com.example.assignment1.model
-
-data class Transaction(
-    val id: Int = 0,  // Auto-increment primary key
-    val description: String,
-    val value: Double = 0.0, // Default value to 0.0
-    val date: String = "", // Default to empty string if not provided
-    val category: String
-)
